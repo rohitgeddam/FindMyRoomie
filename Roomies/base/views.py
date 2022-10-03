673a7606ca0157824448e7f2d73eee781dd6a2cb
shandlermason <samason1@aggies.ncat.edu>
2022-10-03 16:24:12 -0400

    created form test

--- Roomies/base/views.py
+++ Roomies/base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import PersonForm
+from .models import Person
 
 
 # Create your views here.
@@ -8,8 +9,13 @@ def home(request):
 
 
 def profile(request):
+    if request.method == "POST":
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            form.save()
+    person = Person.objects.all()
     form = PersonForm()
-    return render(request, "profile.html", {"form": form})
+    return render(request, "profile.html", {"form": form, "profiles": person})
 
 
 def matches(request):
