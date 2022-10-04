dd3054fc604bd60b23c9edc164261581e7f2c77a
rohitgeddam <rohitgeddam2018@gmail.com>
2022-10-04 09:48:27 -0400

    -

--- src/base/urls.py
+++ src/base/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+<<<<<<< HEAD
     path("", views.home, name="home"),
     path("profile/", views.profile, name="profile"),
     path("findpeople/", views.findpeople, name="findpeople"),
@@ -13,3 +14,10 @@ urlpatterns = [
 
 ]
 
+=======
+    path("profile/", views.profile, name="profile"),
+    path("findpeople/", views.findpeople, name="findpeople"),
+    path("myroom/", views.myroom, name="myroom"),
+    path("", views.home, name="home"),
+]
+>>>>>>> 1c74477 (-)
