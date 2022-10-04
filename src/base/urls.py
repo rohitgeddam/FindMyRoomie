af40acaeca05ab8632f6bf0e1368ffeedfceeed9
rohitgeddam <rohitgeddam2018@gmail.com>
2022-10-04 09:23:55 -0400

    refactor

--- src/base/urls.py
+++ src/base/urls.py
@@ -6,18 +6,8 @@ from . import views
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
-<<<<<<< HEAD
-    path("", views.home, name="home"),
-    path("profile/", views.profile, name="profile"),
-    path("findpeople/", views.findpeople, name="findpeople"),
-    path("myroom/", views.myroom, name="myroom"),
-
-]
-
-=======
     path("profile/", views.profile, name="profile"),
     path("findpeople/", views.findpeople, name="findpeople"),
     path("myroom/", views.myroom, name="myroom"),
     path("", views.home, name="home"),
 ]
->>>>>>> 1c74477 (-)
