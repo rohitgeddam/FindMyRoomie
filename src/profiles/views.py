d76a605760f5822f1a9f25789bfb2752bdd47689
rohitgeddam <rohitgeddam2018@gmail.com>
2022-10-04 09:01:14 -0400

    add myprofile route

---
+++ src/profiles/views.py
@@ -0,0 +1,18 @@
+
+# accounts/views.py
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+from django.shortcuts import render
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
+    
+
+
+def myprofile(request):
+
+    return render(request, 'profiles/myprofile.html')
