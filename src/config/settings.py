21def414902c5356e637ed19bd873fe94ecbfd73
shandlermason <samason1@aggies.ncat.edu>
2022-10-04 02:54:18 -0400

    still working on this DO NOT DELETE

--- src/config/settings.py
+++ src/config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
 
-    'profiles',
+    'base',
 ]
 
 MIDDLEWARE = [
--- src/config/urls.py
+++ src/config/urls.py
@@ -22,7 +22,7 @@ def index(request):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("profiles.urls")),  # new
+    path("accounts/", include("base.urls")),  # new
     path("accounts/", include("django.contrib.auth.urls")),
     path('', index, name="index")
 ]
