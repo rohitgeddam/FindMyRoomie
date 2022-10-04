dd7eaf189f33e7045e29b2a025bc1a99fbc0011b
shandlermason <samason1@aggies.ncat.edu>
2022-10-04 19:56:58 -0400

    deleted Roomies (duplicate project)

--- Roomies/base/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class Person(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.CharField(max_length=100)
-    hometown = models.CharField(max_length=100)
--- Roomies/base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("profile/", views.profile, name="profile"),
-    path("matches/", views.matches, name="matches"),
-]
