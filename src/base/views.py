5e9c2e3b336d42a6336ba920d9545d90a657395b
rohitgeddam <rohitgeddam2018@gmail.com>
2022-10-05 11:37:45 -0400

    add login required for views

--- src/base/views.py
+++ src/base/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from .forms import ProfileForm
 from .models import Profile
  
@@ -16,10 +17,11 @@ class SignUpView(generic.CreateView):
     template_name = "registration/signup.html"
  
  
+
 def home(request):
     return render(request, "index.html")
  
- 
+@login_required() 
 def profile(request):
     if request.method == "POST":
         form = ProfileForm(request.POST)
@@ -30,17 +32,17 @@ def profile(request):
     form = ProfileForm()
     return render(request, "pages/profile.html", {"form": form, "profiles": person})
  
- 
+@login_required() 
 def findpeople(request):
     qs = Profile.objects.filter(visibility=True)
     f = ProfileFilter(request.GET, queryset=qs)
     return render(request, "pages/findpeople.html", {"filter": f})
  
- 
+@login_required() 
 def myroom(request):
     return render(request, "pages/myroom.html")
  
- 
+
 def user_logout(request):
     logout(request)
     messages.error(request, "Logged out successfully!")
