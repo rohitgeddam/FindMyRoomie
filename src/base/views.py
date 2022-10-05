3374650e016f4a72ed17388a28e4c89bd4c5c408
rohitgeddam <rohitgeddam2018@gmail.com>
2022-10-05 11:54:33 -0400

    modify profile model to incorporate null values

--- src/base/views.py
+++ src/base/views.py
@@ -7,41 +7,43 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from .forms import ProfileForm
 from .models import Profile
- 
+
 from .filters import ProfileFilter
- 
- 
+
+
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
- 
- 
+
 
 def home(request):
     return render(request, "index.html")
- 
-@login_required() 
+
+
+@login_required()
 def profile(request):
     if request.method == "POST":
         form = ProfileForm(request.POST)
         if form.is_valid():
             form.save()
- 
+
     person = Profile.objects.all()
     form = ProfileForm()
     return render(request, "pages/profile.html", {"form": form, "profiles": person})
- 
-@login_required() 
+
+
+@login_required()
 def findpeople(request):
     qs = Profile.objects.filter(visibility=True)
     f = ProfileFilter(request.GET, queryset=qs)
     return render(request, "pages/findpeople.html", {"filter": f})
- 
-@login_required() 
+
+
+@login_required()
 def myroom(request):
     return render(request, "pages/myroom.html")
- 
+
 
 def user_logout(request):
     logout(request)
