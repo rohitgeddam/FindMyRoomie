6b5955fb6d84d1fed7412e1230e78d457a415026
Darkviper7 <m.s.k.tejavarma@gmail.com>
2022-10-05 00:27:28 -0400

    Add a toast message

--- src/base/views.py
+++ src/base/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
@@ -5,37 +6,38 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from .forms import ProfileForm
 from .models import Profile
-
-
+ 
+ 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
-
-
+ 
+ 
 def home(request):
     return render(request, "index.html")
-
-
+ 
+ 
 def profile(request):
     if request.method == "POST":
         form = ProfileForm(request.POST)
         if form.is_valid():
             form.save()
-
+ 
     person = Profile.objects.all()
     form = ProfileForm()
     return render(request, "pages/profile.html", {"form": form, "profiles": person})
-
-
+ 
+ 
 def findpeople(request):
     return render(request, "pages/findpeople.html")
-
-
+ 
+ 
 def myroom(request):
     return render(request, "pages/myroom.html")
-
-
+ 
+ 
 def user_logout(request):
     logout(request)
+    messages.error(request, "Logged out successfully!")
     return redirect("home")
