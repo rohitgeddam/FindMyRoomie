c8d3855b96f41eb8900186ee8d6bd004ffd26f0b
Arun Kumar <rarunkumar15112k@gmail.com>
2022-10-06 01:35:20 -0400

    Update test_views.py

--- src/tests/test_views.py
+++ src/tests/test_views.py
@@ -2,20 +2,6 @@ from django.test import SimpleTestCase, TestCase, Client
 from django.urls import reverse, resolve
 from base.views import home, profile, findpeople, myroom
 import json
-
-class TestUrls(SimpleTestCase):
-    def test_url(self):
-        url=reverse('home')
-        self.assertEquals(resolve(url).func,home)
-    def test_profile_url(self):
-        url=reverse('profile')
-        self.assertEquals(resolve(url).func,profile)
-    def test_findpeople_url(self):
-        url=reverse('findpeople')
-        self.assertEquals(resolve(url).func,findpeople)    
-    def test_myroom_url(self):
-        url=reverse('myroom')
-        self.assertEquals(resolve(url).func,myroom)
         
 class TestViews(TestCase):
     def test_home(self):
