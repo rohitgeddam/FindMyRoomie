d9dacbf1ab105f7de95387c1c156bc51b41c9844
Darkviper7 <m.s.k.tejavarma@gmail.com>
2022-10-07 02:23:22 -0400

    fix lint

--- src/tests/test_views.py
+++ src/tests/test_views.py
@@ -1,6 +1,7 @@
-from django.test import SimpleTestCase, TestCase, Client
-from django.urls import reverse, resolve
-from base.views import home, profile, findpeople, myroom
+from django.test import TestCase, Client
+from django.urls import reverse
+
+# from base.views import home, profile, findpeople, myroom
 
 # from unittest.mock import patch, MagicMock
 # import mock
