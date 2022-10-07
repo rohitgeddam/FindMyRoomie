82679f0a845425b04b39f6b388e4346ce689319d
Darkviper7 <m.s.k.tejavarma@gmail.com>
2022-10-06 23:20:23 -0400

    Add init function to profile form

--- src/base/forms.py
+++ src/base/forms.py
@@ -3,6 +3,11 @@ from .models import Profile
 
 
 class ProfileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        for bound_field in self:
+            if (hasattr(bound_field, "field") and bound_field.name in self.Meta.required_fields):bound_field.field.widget.attrs["required"] = "required"
+
     class Meta:
         model = Profile
         fields = (
