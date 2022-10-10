a4c1b1d55c6f5d142531182d6f1d499fe1936994
rohitgeddam <rohitgeddam2018@gmail.com>
2022-10-09 21:37:32 -0400

    add about page

--- src/base/urls.py
+++ src/base/urls.py
@@ -26,6 +26,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from base.views import ActivateAccount
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -35,6 +36,11 @@ urlpatterns = [
     path("findpeople/", views.findpeople, name="findpeople"),
     path("myroom/", views.myroom, name="myroom"),
     path("logout/", views.user_logout, name="user_logout"),
+    path(
+        "about",
+        TemplateView.as_view(template_name="pages/about.html"),
+        name="about",
+    ),
     path(
         "activate/<uidb64>/<token>/",
         ActivateAccount.as_view(),
